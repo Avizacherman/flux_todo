f78f255405af425a5bb9b0321818b79d73966200
Avi Zacherman <avizacherman@gmail.com>
2016-03-25 09:38:56 -0400

    YAAAAS

--- tests/route_tests/api/show_test.js
+++ tests/route_tests/api/show_test.js
@@ -6,20 +6,36 @@ require('../../helpers/test_helpers.js')()
 
 describe("Show Routes", () => {
 
+  let req, res, client, connectStub
 
-  it("Should retrieve data from the DB", function(done){
-    let req = {params: {id: 1}}
-    let res = { data: {},
+  beforeEach( () => {
+    // Mocking request/response cycle
+    // Response object has placeholder to hold data for inspection
+    req = {params: {id: 1}}
+    res = { data: {},
       json: function(obj){
-        this.data = obj }
+      this.data = obj }
     }
-    show(req, res)
-    setTimeout(function() {
-      expect(res.data.id).to.eq(1)
-      expect(res.data.subject).to.eq('Do it')
-      expect(res.data).to.be.an('array')
-      done()
-    }, 100)
+
+    // create some fake data that would come back from the database
+    // The query is a stub that provides its callback with this fake data
+    // the connection is a stub that provides the client stub
+    let fakeData = { rows: [{id: 1, subject: 'Do it'}] }
+    let queryStub = stub()
+    client = { query: queryStub.callsArgWith(2, null, fakeData) }
+    connectStub = stub(pg, 'connect').yields(null, client, spy())
+
   })
 
+  afterEach( () => {
+    pg.connect.restore()
+  })
+
+  it("Should retrieve data from the DB", (done) => {
+    show(req, res)
+    expect(res.data.id).to.eq(1)
+    expect(res.data.subject).to.eq('Do it')
+    expect(res.data).to.be.an('object')
+    done()
+  })
 })
--- tests/route_tests/api/update_test.js
+++ tests/route_tests/api/update_test.js
@@ -5,13 +5,19 @@ let update = require ('../../../routes/api/update.js')
 require('../../helpers/test_helpers.js')()
 
 describe("Update Route Mocks", () => {
+  let client, connectStub, reqMock
 
+  beforeEach(() => {
+    client = { query: spy() }
+    connectStub = stub(pg, 'connect').yields(null, client)
+    reqMock = {body: {subject: "test", details: "test", completed: true}, params: {id: 1}}
+  })
+
+  afterEach(() => {
+    pg.connect.restore()
+  })
 
   it("Should call a DB Query", () => {
-    let client = { query: spy() }
-    let connectStub = stub(pg, 'connect').yields(null, client)
-    let reqMock = {body: {subject: "test", details: "test", completed: true}, params: {id: 1}}
-
     update(reqMock)
     expect(connectStub.callCount).to.eq(1)
     expect(client.query.callCount).to.eq(1)
